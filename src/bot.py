d0215c741f9777b86469f4b577a45426656488fe
Antal Kenderesi <43848302+alkenderesi@users.noreply.github.com>
2023-12-21 14:29:35 +0100

    Add poll submissions

--- src/bot.py
+++ src/bot.py
@@ -23,7 +23,7 @@ class NextGameBot:
         Handles messages sent to the bot.
 
         Args:
-            * message (discord.Message): The message sent to the bot.
+            * message (`discord.Message`): The message sent to the bot.
         """
 
         if message.author == self.client.user:
@@ -37,6 +37,14 @@ class NextGameBot:
         ):
             await self._start_poll(message)
 
+        elif (
+            self.game_poll
+            and message.channel.type.name == "private"
+            and message.author in self.game_poll.participants
+            and message.author not in self.game_poll.submissions
+        ):
+            await self._submit_poll_response(message)
+
     async def _start_poll(self, message: discord.Message) -> None:
         """
         Starts a new game poll.
@@ -46,7 +54,7 @@ class NextGameBot:
         and sending the game options to them.
 
         Args:
-            * message (discord.Message): The message sent to the bot with the
+            * message (`discord.Message`): The message sent to the bot with the
               participants of the poll.
         """
 
@@ -57,9 +65,31 @@ class NextGameBot:
         )
 
         for participant in self.game_poll.participants:
-            if participant.dm_channel:
-                async for dm in participant.dm_channel.history():
-                    if dm.author == self.client.user:
-                        await dm.delete()
+            dm_channel = await participant.create_dm()
+            async for dm in dm_channel.history():
+                if dm.author == self.client.user:
+                    await dm.delete()
 
             await participant.send(templates.options_message(self.game_poll.games))
+
+    async def _submit_poll_response(self, message: discord.Message) -> None:
+        """
+        Submits a response to the poll.
+
+        This includes sending a confirmation message to the participant,
+        and updating the progress message in the main channel.
+
+        Args:
+            * message (`discord.Message`): The message sent to the bot with the
+              participant's response.
+        """
+
+        await message.channel.send(
+            templates.confirmation_message(self.game_poll.submit(message))
+        )
+
+        await self.info_message.edit(
+            content=templates.progress_message(
+                len(self.game_poll.submissions), len(self.game_poll.participants)
+            )
+        )
--- src/poll.py
+++ src/poll.py
@@ -11,3 +11,27 @@ class GamePoll:
         self.submissions: dict[discord.Member, tuple[int, list[str]]] = {}
         with open("config/games.txt", "r") as game_config:
             self.games = game_config.read().splitlines()
+
+    def submit(self, message: discord.Message) -> list[str]:
+        """
+        Submits a response to the poll.
+
+        Args:
+            * message (`discord.Message`): The message to submit.
+
+        Returns:
+            * `list[str]`: The list of games in the order they were found in the
+              message.
+        """
+
+        game_indices = {
+            idx: game
+            for game in self.games
+            if (idx := message.content.find(game)) != -1
+        }
+
+        games = [game_indices[idx] for idx in sorted(game_indices.keys())]
+
+        self.submissions[message.author] = (len(self.submissions), games)
+
+        return games
