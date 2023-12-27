78266f38f1f7a1a5e6cbd8b5e472cccd23a8b344
Antal Kenderesi <43848302+alkenderesi@users.noreply.github.com>
2023-12-27 20:23:02 +0100

    Update bot without templates

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,8 @@
-"""Bot logic module."""
-
 import discord
+from poll import Poll
 
 
-class PollBot():
-    """Discord bot for poll creation and management."""
-
+class PollBot:
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, "_instance"):
             cls._instance = super().__new__(cls)
@@ -13,13 +10,63 @@ class PollBot():
 
     def __init__(self, client: discord.Client) -> None:
         self.client = client
+        self.poll = Poll()
+        self.main_channel: discord.TextChannel = None
+        self.poll_dms: dict[discord.Member, discord.Message] = {}
+        self.turn_dms: dict[discord.Member, discord.Message] = {}
 
     async def message_handler(self, message: discord.Message) -> None:
-        """
-        Handles messages sent to the bot.
+        if message.author == self.client.user:
+            return
 
-        Args:
-            * message (`discord.Message`): The message sent to the bot.
-        """
+        if (
+            not self.poll.is_active()
+            and not self.poll.is_complete()
+            and message.channel.type.name == "text"
+            and message.content.startswith("!nextgame")
+            and message.mentions
+            and self.client.user not in message.mentions
+        ):
+            await self.start(message)
 
-        pass
+        elif (
+            self.poll.is_active()
+            and message.channel.type.name == "private"
+            and message.author == self.poll.active_participant
+            and message.content in self.poll.games
+        ):
+            await self.submit(message)
+
+        if self.poll.is_complete():
+            await self.main_channel.send(self.poll.result())
+            self.reset()
+
+    async def start(self, message: discord.Message) -> None:
+        self.poll.add(message.mentions)
+        self.main_channel = message.channel
+
+        for participant in self.poll.participants:
+            self.poll_dms[participant] = await participant.send("")
+
+            if participant == self.poll.active_participant:
+                self.turn_dms[participant] = await participant.send("")
+
+            else:
+                self.turn_dms[participant] = await participant.send("")
+
+    async def submit(self, message: discord.Message):
+        last_active = self.poll.active_participant
+        self.poll.submit(message.content)
+
+        await self.turn_dms[last_active].edit(content="")
+        await self.turn_dms[self.poll.active_participant].edit(content="")
+
+        poll_message = ""
+        for participant in self.poll.participants:
+            await self.poll_dms[participant].edit(content=poll_message)
+
+    def reset(self):
+        self.poll.reset()
+        self.main_channel = None
+        self.poll_dms.clear()
+        self.turn_dms.clear()
